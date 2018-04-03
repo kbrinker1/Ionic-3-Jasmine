12844567db217dce042b099a4364fe50d379efed
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-04-03 13:04:43 -0400

    All dependencies added, 'npm test' is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MyProvider } from '../providers/my/my';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MyProvider
   ]
 })
 export class AppModule {}
