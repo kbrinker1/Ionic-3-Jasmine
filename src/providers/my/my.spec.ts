96cace0388f42cda77a186bcb98a9fee3b242e13
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-04-03 13:37:44 -0400

    Wrote first test, updated zone.js, got it to pass

---
+++ src/providers/my/my.spec.ts
@@ -0,0 +1,14 @@
+import { MyProvider } from './my';
+import { HttpClient } from '@angular/common/http';
+ 
+let test = null;
+ 
+describe('basic test', () => {
+ 
+    it('should return true', () => {
+ 
+            expect(true).toBeTruthy;
+            
+        }
+    );
+});
